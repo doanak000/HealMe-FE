9b6c6f276f099ba554607bcf2bf14385028ef8e2
doanak000 <doanak000@gmail.com>
2023-04-21 11:40:03 +0700

    optimize/refecthdata-each-change-tab

--- src/components/Profile/ProfileContent/ProfileContent.js
+++ src/components/Profile/ProfileContent/ProfileContent.js
@@ -1,5 +1,5 @@
 import { Tabs } from "antd";
-import React from "react";
+import React, { useState } from "react";
 import ProfileDetail from "../ProfileDetail/ProfileDetail";
 import WorkSchedular from "../WorkSchedular/WorkSchedular";
 import { memo } from "react";
@@ -7,46 +7,66 @@ import PatientAppointment from "../PatientAppointment/PatientAppointment";
 import WorkSchedularManage from "../WorkSchedularManage/WorkSchedularManage";
 
 const ProfileContent = () => {
-  const onChange = (key) => {
-    console.log(key);
-  };
+  const [activeTab, setActiveTab] = useState("1");
   const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+
+  const handleChangeTab = (key) => {
+    setActiveTab(key);
+  };
+
+  const renderContent = () => {
+    switch (activeTab) {
+      case "1":
+        return <ProfileDetail />;
+      case "2":
+        return userInfo.role_id === 2 ? (
+          <PatientAppointment />
+        ) : (
+          <WorkSchedular />
+        );
+      case "3":
+        return <WorkSchedularManage />;
+      default:
+        return null;
+    }
+  };
+
   const items = [
     {
       key: "1",
       label: `Thông tin cá nhân`,
-      children: <ProfileDetail />,
     },
     {
       key: "2",
       label: `Lich sử đơn hàng`,
-      children: <PatientAppointment />,
     },
   ];
+
   const itemsOfDoctor = [
     {
       key: "1",
       label: `Thông tin cá nhân`,
-      children: <ProfileDetail />,
     },
     {
       key: "2",
       label: `Đăng ký lịch khám`,
-      children: <WorkSchedular />,
     },
     {
       key: "3",
       label: "Quản lý lịch khám",
-      children: <WorkSchedularManage />,
     },
   ];
+
+  const tabItems = userInfo.role_id === 2 ? items : itemsOfDoctor;
+
   return (
-    <Tabs
-      defaultActiveKey="1"
-      items={userInfo.role_id == 2 ? items : itemsOfDoctor}
-      onChange={onChange}
-      size="large"
-    />
+    <Tabs activeKey={activeTab} onChange={handleChangeTab} size="large">
+      {tabItems.map(({ key, label }) => (
+        <Tabs.TabPane key={key} tab={label}>
+          {renderContent()}
+        </Tabs.TabPane>
+      ))}
+    </Tabs>
   );
 };
 
